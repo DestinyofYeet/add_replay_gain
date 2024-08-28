f4cc46c39b9efc18d395a9b86556d0d09873e789
DestinyofYeet <olebend@gmail.com>
2024-08-28 22:27:49 +0200

    removed self from module

--- flake.nix
+++ flake.nix
@@ -5,7 +5,9 @@
     nixpkgs.url = "github:nixos/nixpkgs?ref=nixos-unstable";
   };
 
-  outputs = { self, nixpkgs }: {
+  outputs = { self, nixpkgs }: let
+  pkgs = import nixpkgs { system = "x86_64-linux"; }; 
+in {
 
     packages.x86_64-linux.add-replay-gain = nixpkgs.lib.callPackage ./pkg.nix {};
 
