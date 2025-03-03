a76a28dbcab0558bd44af2f525006f69364c6ee5
DestinyofYeet <ole@ole.blue>
2025-03-03 02:16:28 +0100

    fix(pkg): Fixes pkg.nix

--- flake.nix
+++ flake.nix
@@ -1,31 +1,27 @@
 {
   description = "Add replay gain to files";
 
-  inputs = {
-    nixpkgs.url = "github:nixos/nixpkgs?ref=nixos-unstable";
-  };
+  inputs = { nixpkgs.url = "github:nixos/nixpkgs?ref=nixos-unstable"; };
 
-  outputs = { self, nixpkgs }: let
-  pkgs = import nixpkgs { system = "x86_64-linux"; }; 
+  outputs = { self, nixpkgs }:
+    let
+      pkgs = import nixpkgs { system = "x86_64-linux"; };
 
-  forAllSystems = function:
-    nixpkgs.lib.genAttrs [
-      "x86_64-linux"
-      # "aarch64-linux"
-    ] (system: function (import nixpkgs { inherit system; }));
+      forAllSystems = function:
+        nixpkgs.lib.genAttrs [
+          "x86_64-linux"
+          # "aarch64-linux"
+        ] (system: function (import nixpkgs { inherit system; }));
 
-in {
+    in {
 
-    packages = forAllSystems (pkgs:
-      {
-        add-replay-gain = pkgs.callPackage ./pkg.nix {};
-      }
-    );
+      packages = forAllSystems (pkgs: rec {
+        default = pkgs.callPackage ./pkg.nix { };
+        add-replay-gain = default;
+      });
 
-    nixosModules.add-replay-gain = import ./module.nix self;
+      nixosModules.add-replay-gain = import ./module.nix self;
 
-    hydraJobs = {
-      inherit (self) packages;
-    };   
-  };
+      hydraJobs = { inherit (self) packages; };
+    };
 }
--- pkg.nix
+++ pkg.nix
@@ -1,32 +1,29 @@
-{ 
-  rustPlatform,
-  lib,
-  fetchFromGitHub,
-  pkgs,
-}: 
+{ rustPlatform, lib, fetchFromGitHub, pkgs, }:
 
 rustPlatform.buildRustPackage rec {
   pname = "add_replay_gain_to_files";
   version = "1.7";
 
-  src = fetchFromGitHub {
-    owner = "DestinyofYeet";
-    repo = "add_replay_gain";
-    rev = "fbcee0534694ad057bdc273cd5b7476ad718a13c";
-    hash = "sha256-N8Bkg5IuIkQnQhTUbKEEzaCZMLlHxZqTnDJ6CR6wxmI=";
-  };
+  # src = fetchFromGitHub {
+  #   owner = "DestinyofYeet";
+  #   repo = "add_replay_gain";
+  #   rev = "fbcee0534694ad057bdc273cd5b7476ad718a13c";
+  #   hash = "sha256-N8Bkg5IuIkQnQhTUbKEEzaCZMLlHxZqTnDJ6CR6wxmI=";
+  # };
+
+  src = ./.;
 
   cargoHash = "sha256-e0bgEAmJNKRWVjSMlb3qPIjjk16UDNGbpBGKXzWKHbE=";
 
-  nativeBuildInputs = with pkgs; [
-    pkg-config
-    openssl.dev
-  ];
+  useFetchCargoVendor = true;
+
+  nativeBuildInputs = with pkgs; [ pkg-config openssl.dev ];
 
   PKG_CONFIG_PATH = "${pkgs.openssl.dev}/lib/pkgconfig";
 
   meta = with lib; {
-    description = "A tool to automatically add replay gain to mp3 and flac files";
+    description =
+      "A tool to automatically add replay gain to mp3 and flac files";
     license = licenses.gpl2;
     # maintainers = [ maintainers.DestinyofYeet ];
     platforms = platforms.all;
